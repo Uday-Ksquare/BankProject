39731b8a51113cc7b32799a00b20180a0f13d560
Uday-Ksquare <uday.valapudasu@theksquaregroup.com>
2025-08-21 10:46:40 +0530

    import changed

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 // import './index.css'
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import Layout from "./layouts/dashboard.jsx";
+import Layout from "./layouts/Dashboard.jsx";
 import SupplementsL1 from "./components/SupplementsL1.jsx";
 import SupplementsL2 from "./components/SupplementsL2.jsx";
 import DocumentUploaderpage from "./pages/DocumentuploaderPage.jsx";
